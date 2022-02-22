6bf20448126dd97b72d666bb3d1abb1fa5a4ebb5
edigiacomoIBM <99971218+edigiacomoIBM@users.noreply.github.com>
2022-02-22 15:07:26 -0300

    Metodo Post

--- automationRestAssuredIbmGalicia/src/test/java/back/step/testPruebaEze.java
+++ automationRestAssuredIbmGalicia/src/test/java/back/step/testPruebaEze.java
@@ -1,6 +1,8 @@
 package back.step;
 
 import static org.junit.Assert.assertEquals;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.hamcrest.Matchers.equalTo;
 
 import org.junit.Test;
 
@@ -9,6 +11,7 @@ import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
+import mapeo.Usuario;
 import util.Url;
 
 public class testPruebaEze {
@@ -34,6 +37,36 @@ public class testPruebaEze {
                 extract().response();
 
         assertEquals("El Status Code es incorrecto: " + response.getStatusCode(), 200, response.statusCode());
+
+    }
+
+    @Test
+    public void testPost(){
+        
+        String name = "Lautaro";
+        String job = "Programador";
+
+        requestSpecification = new RequestSpecBuilder().
+                setBaseUri(Url.URL_API).
+                setContentType(ContentType.JSON).
+                build();
+
+        Usuario requestBody = (new Usuario(name, job));
+
+        response = 
+            given().
+                spec(requestSpecification).
+                body(requestBody).
+            when().
+                post("/users").
+            then().
+                log().all().
+                body(matchesJsonSchemaInClasspath("schemas/schemaPost.json")).
+                body("name", equalTo(name)).
+                body("job", equalTo(job)).
+                extract().response();
+
+        assertEquals("El Status Code es incorrecto: " + response.getStatusCode(), 201, response.statusCode());
 
     }
 }
